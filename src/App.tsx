8aec7573e5a03e3abbc3b9fc1779b3ce5d4b03a2
JACKSPARROWbts <18tucs221@skct.edu.in>
2020-11-06 21:05:16 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect,useState} from 'react'
 
 function App() {
+  let apiresponse:any;
+  var[state,setState]=useState(apiresponse="");
+  var[condition,setCondition]=useState(apiresponse="");
+  const[name,setName]=useState("");
+  const[pass,setPass]=useState("")
+  useEffect(()=>{
+    callAPI();
+  })
+  const callAPI=()=>{
+    fetch('http://localhost:3001')
+    .then(res=>res.text())
+    .then(res=>{
+      setState(apiresponse=res);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
+  const save=(e:any)=>{
+    if(e.target.id==="name"){
+   setName(e.target.value);
+    }else{
+      setPass(e.target.value);
+    }
+  }
+  const submit=async(e:any)=>{
+    e.preventDefault();
+    fetch('http://localhost:3001',{
+      method:"post",
+      body:JSON.stringify({
+        "name":name,
+        "password":pass
+      })
+    })
+    .then(res=>res.text())
+    .then(res=>{
+      setCondition(apiresponse=res);
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form>
+      <input type="text" name="name" id="name" onChange={save} ></input>
+      <br></br>
+      <input type="text" name="password" id="password" onChange={save} ></input>
+      <br></br>
+      <button type="submit" onClick={submit}>submit</button>
+        </form>
+      {state}
+      {condition}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
